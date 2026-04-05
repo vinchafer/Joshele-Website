68dabde413d084b6732b1af476a99065dae40e2f
vinchafner <vinc.hafner@gmail.com>
2026-04-05 14:43:20 -0500

    fix: add tsconfig.json for @/ path aliases, update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'files.designer.hoststar.ch',
       },
     ],
+    unoptimized: true,
   },
 }
 
